bcf0c5f9bb748e2166716d39e8e93881bf8a0321
21TekSavvvy <jareddarochajj@gmail.com>
2022-02-10 01:19:26 -0800

    .

---
+++ Project6/Project6/Program.cs
@@ -0,0 +1,31 @@
+﻿class LargestNumber
+{
+    static void Main()
+    {
+        //hardcoded 3 numbers
+        int num1 = 60;
+        int num2 = 45;
+        int num3 = 123;
+
+        // declare variables
+        int large = 0;
+
+        //Check if Number 1 is larger the other 2
+        if(num1 > num2 && num1 > num3)
+        {
+           large = num1;
+        }
+        //Check if Number 2 is larger the other 2
+        else if (num2 > num3&& num2 > num1)
+        {
+            large = num2;
+        }
+        //Check if Number 3 is larger the other 2
+        else
+        {
+            large = num3;
+        }
+        System.Console.WriteLine(large);
+        System.Console.ReadKey();
+    }
+}
